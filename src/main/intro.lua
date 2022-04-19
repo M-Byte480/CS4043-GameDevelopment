603d824fcbba7cff79b9b38d43e12d113060242c
Italo <92447706+italotrt@users.noreply.github.com>
2022-04-20 00:44:15 +0100

    Improved intro, with the texts fixed

--- src/main/intro.lua
+++ src/main/intro.lua
@@ -1,10 +1,23 @@
+-- Intro:
+-- (done by Italo)
+
 local composer = require( "composer" )
 
 local scene = composer.newScene()
 
-
 local snapshot = display.newSnapshot( 4000, 4000)
 halfH = display.contentHeight * 0.5
+
+-- Pressing Space to skip the intro
+local function onKeyEvent( event )
+   if ( "space" == event.keyName and event.phase == "down" ) then
+      composer.gotoScene( "game")
+   end
+ 
+   return true
+end
+
+local skip = "Press Space to skip"
 
 local story =   [[
 Your name is David Silver Kinsella,
@@ -59,93 +72,53 @@ The creature is now dead, but you notice…
 
 
 You are not alone...
-
-Press Space to Continue
                 ]]
 
+-- -----------------------------------------------------------------------------------
+-- Displaying Section
+-- -----------------------------------------------------------------------------------
 local options = -- Display settings for the text
 {
    text = story,
-   x = 100,
+   x = 150,
    y = halfH,
    font = native.systemFont,
    fontSize = 20,
    align = "center"
 }
 
-local storyText = display.newText( options ) -- Display the story text
+local function introVideo()
+   -- Moves the text upwards
+   transition.to( snapshot.path, { 
+   time = 80000, 
+   x1 = 0, y1 = -display.contentHeight, 
+   x2 = 0, y2 = -display.contentHeight * 2, 
+   x3 = 0, y3 = -display.contentHeight * 2,  
+   x4 = -0, y4= -display.contentHeight } )
+end
+
+local skipText = display.newText( skip, 1400, 125, native.systemFont, 25 ) -- Display the skip text
+local storyText = display.newText( options )                              -- Display the story text
 
 storyText:setFillColor( 0.8, 0.6, 0.4 ) -- Colour of the text
-storyText:translate( 250, 440) -- Position of the text
-
-snapshot.group:insert( storyText ) -- Insert storyText into snapshot
-snapshot:translate( 0, halfH )  -- Center snapshot
-snapshot:invalidate()           -- Invalidate snapshot
-snapshot:scale( 2, 2 )
-
-local function introVideo()
-    
-
--- Moves the text upwards
-transition.to( snapshot.path, { 
-time = 80000, 
-x1 = 0, y1 = -display.contentHeight * 1.7, 
-x2 = 0, y2 = -display.contentHeight * 2, 
-x3 = 0, y3 = -display.contentHeight * 2,  
-x4 = -0, y4= -display.contentHeight * 1.7 } )
-
-end
--- -- Initialize variables
-local function gotoGame()
-    composer.gotoScene( "game", { time=800, effect="crossFade" } )
-end
-
-local onComplete = function( event )
-   composer.gotoScene( "game", { time=800, effect="crossFade" } )
-end
+storyText:translate( 250, 440)          -- Position of the text
+snapshot.group:insert( storyText )      -- Insert storyText into snapshot
+snapshot:translate( 0, halfH )          -- Center snapshot
+snapshot:invalidate()                   -- Invalidate snapshot
+snapshot:scale( 2, 2 )                  -- Scale snapshot
 
 function scene:show( event )
-    introVideo()
+   introVideo()
 end
+-- -----------------------------------------------------------------------------------
 
--- -- Pressing Space to skip the intro
-local function onKeyEvent( event )
-   if ( "space" == event.keyName and event.phase == "down" ) then
-      gotoGame()
-   end
 
-   return true
-end
-
--- hide()
-function scene:hide( event )
-
-    local sceneGroup = self.view
-    local phase = event.phase
-if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
-
-    elseif ( phase == "did" ) then
-        -- Code here runs immediately after the scene goes entirely off screen
-    end
-
-end
-
--- destroy()
-function scene:destroy( event )
-
-    local sceneGroup = self.view
-    -- Code here runs prior to the removal of scene's view
-
-end
 -- -----------------------------------------------------------------------------------
 -- Scene event function listeners
 -- -----------------------------------------------------------------------------------
 Runtime:addEventListener( "key", onKeyEvent )
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
 -- -----------------------------------------------------------------------------------
 
 return scene
